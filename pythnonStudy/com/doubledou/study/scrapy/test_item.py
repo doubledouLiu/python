defc5e3539815d065ca0b28821cd9878ca453c52
liudoudou <liudoudou@coocaa.com>
2018-01-29 18:23:08 +0800

    scrapy使用

---
+++ pythnonStudy/com/doubledou/study/scrapy/test_item.py
@@ -0,0 +1,25 @@
+import scrapy
+
+
+class Product(scrapy.Item):
+    name = scrapy.Field()
+    price = scrapy.Field()
+    stock = scrapy.Field()
+    last_updated = scrapy.Field(serializer=str)
+
+
+if __name__ == '__main__':
+     print(Product.fields)
+     product = Product(name='Desktop PC', price=1000)
+     print (product)
+     print (product['name'])
+     print (product.get('name'))
+     #print (product['last_updated'])
+     print (product.get('stock'))
+     print (product.get('stock', 'not set'))
+     print ('name' in product)
+     print ('stock' in product)
+     print ('stock' in product.fields)
+     print (product.get('stock'))
+     product['last_updated'] = 'today'
+     print (product['last_updated'])
