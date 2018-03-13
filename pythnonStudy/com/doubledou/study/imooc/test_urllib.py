544373ad1187e744c78a801c5ca5777b8d0e4381
18320781961@163.com <18320781961@163.com>
2018-03-13 21:30:30 +0800

    add py file about class-object

--- pythnonStudy/com/doubledou/study/imooc/test_urllib.py
+++ pythnonStudy/com/doubledou/study/imooc/test_urllib.py
@@ -7,27 +7,27 @@ URL_GET = 'http://httpbin.org/get'
 
 def use_simple_urllib2():
     response = urllib2.urlopen(URL_IP)
-    print '>>Response Headers:'
-    print response.info()
-    print '>>>Response Body:'
-    print ''.join([line for line in response.readlines()])
+    print('>>Response Headers:')
+    print (response.info())
+    print ('>>>Response Body:')
+    print(''.join([line for line in response.readlines()]))
 
 
 def use_params_urllib2():
     params = urllib.urlencode({'param1':'hello','param2':'world'})
-    print 'Request Params:'
-    print params
+    print('Request Params:')
+    print (params)
     response = urllib2.urlopen('?'.join([URL_GET, '%s']) % params)
-    print '>>Response Headers:'
-    print response.info()
-    print '>>>Status Code:'
-    print response.getcode()
-    print '>>>Response Body:'
-    print ''.join([line for line in response.readlines()])
+    print ('>>Response Headers:')
+    print (response.info())
+    print ('>>>Status Code:')
+    print (response.getcode())
+    print ('>>>Response Body:')
+    print (''.join([line for line in response.readlines()]))
 
 
 if __name__ == '__main__':
-    print '>>>Use simple urllib2:'
+    print ('>>>Use simple urllib2:')
     use_simple_urllib2()
-    print '>>>User params urllib2:'
+    print ('>>>User params urllib2:')
     use_params_urllib2()
--- pythnonStudy/com/doubledou/study/srapy/test_item.py
+++
@@ -1,8 +0,0 @@
-import scrapy
-
-
-class Product(scrapy.Item):
-    name = scrapy.Field()
-    price = scrapy.Field()
-    stock = scrapy.Field()
-    last_updated = scrapy.Field(serializer=str)
